6837a0e0a61d30abefa675925768109d1882ff90
t-abe-pixel <t.abe.mksc@gmail.com>
2023-11-22 14:28:40 +0900

    remove set google_app_token

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,7 +5,6 @@ export const getEnv = () =>
   const {
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    GOOGLE_API_TOKEN,
     GOOGLE_ACCESS_TOKEN,
     GOOGLE_REFRESH_TOKEN,
     GOOGLE_CALENDAR_ID,
@@ -16,9 +15,6 @@ export const getEnv = () =>
   }
   if (!GOOGLE_CLIENT_SECRET) {
     throw Error('Please set the environment variable GOOGLE_CLIENT_SECRET.');
-  }
-  if (!GOOGLE_API_TOKEN) {
-    throw Error('Please set the environment variable GOOGLE_API_TOKEN.');
   }
   if (!GOOGLE_ACCESS_TOKEN) {
     throw Error('Please set the environment variable GOOGLE_ACCESS_TOKEN.');
@@ -33,7 +29,6 @@ export const getEnv = () =>
   return {
     googleClientID: GOOGLE_CLIENT_ID,
     googleClientSecret: GOOGLE_CLIENT_SECRET,
-    googleApiToken: GOOGLE_API_TOKEN,
     googleAccessToken: GOOGLE_ACCESS_TOKEN,
     googleRefreshToken: GOOGLE_REFRESH_TOKEN,
     googleCalendarID: GOOGLE_CALENDAR_ID,
