c1beb8cc9c8c3b1dbc86057915dbc70e423963fe
t-abe-pixel <t.abe.mksc@gmail.com>
2024-01-04 18:13:54 +0900

    fix translation

--- src/translation.ts
+++ src/translation.ts
@@ -2,17 +2,16 @@ import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 import translationJa from './locales/ja.json';
 import translationEn from './locales/en.json';
+import { getLanguage } from './lib/env';
 
 export const initializeI18n = async () => {
-  const language = process.env.LANG?.split('.')[0] || '';
-
   const resources = {
     ja: { translation: translationJa },
     en: { translation: translationEn },
   };
 
   await i18next.use(Backend).init({
-    lng: language,
+    lng: getLanguage(),
     fallbackLng: 'en',
     resources,
   });
